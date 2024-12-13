01a3b63789cd3c4b1ef1bd9fda1a74f5dfbe926a
Shannon Atkinson <rainmanjam@gmail.com>
2024-12-13 14:23:30 -0800

    fix: downloading model

--- services/v1/video/caption_video.py
+++ services/v1/video/caption_video.py
@@ -47,7 +47,7 @@ def rgb_to_ass_color(rgb_color):
 
 def generate_transcription(video_path, language='auto'):
     try:
-        model = whisper.load_model("base")
+        model = whisper.load_model("turbo")
         transcription_options = {
             'word_timestamps': True,
             'verbose': True,
